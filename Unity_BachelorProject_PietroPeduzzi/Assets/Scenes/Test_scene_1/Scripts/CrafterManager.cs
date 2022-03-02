5f978467a65f251ad2503d649032f6a6b5b84c84
ppeduzzi <93376026+supergrugno@users.noreply.github.com>
2022-03-02 14:27:21 +0100

    Added comments to explain script

--- Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/CrafterManager.cs
+++ Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/CrafterManager.cs
@@ -6,14 +6,18 @@ using System;
 
 public class CrafterManager : MonoBehaviour
 {
+    //blanck item for result
     [SerializeField] private GameObject itemBlueprint;
 
+    //list of all recipes the crafter can handle
     public CraftingRecipesTemplate[] recipes;
 
+    //list of slots the crafter has and other variables for organisation
     public GameObject[] itemSlots;
     public bool crafterIsFull = false;
     private int fullSlotsCounter = 0;
 
+    //list used to group all items in all slots and list used to reset the first list to empty
     public ItemTemplate[] _itemsInSlot;
     public ItemTemplate[] _itemsInSlotVoid;
 
@@ -24,12 +28,15 @@ public class CrafterManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             CompleteRecepy();
+            //after each check the items array is reset
+            //items in the array will appear in same order as the item slots that contain them
             ClearArray();
         }
     }
 
     public void CompleteRecepy()
     {
+        //here the crafter checks if all slots are full
         fullSlotsCounter = 0;
         foreach (var item in itemSlots)
         {
@@ -41,20 +48,24 @@ public class CrafterManager : MonoBehaviour
         }
         if (fullSlotsCounter == itemSlots.Length)
         {
+            //if all crafter slots are full the crafter checks the recepies
             Debug.Log("Crafter is full");
             CheckRecepy();
         }
         else
         {
+            //if the crafter is not full nothing happens
             Debug.Log("Crafter need to be filled");
         }
     }
     
     private void CheckRecepy()
     {
+        //crafter goes thru all recipes and checks if they have the needed elements
         foreach (var item in recipes)
         {
             bool isEqual = item.itemsNeeded.SequenceEqual(_itemsInSlot);
+            //if the elements in the crafter are the same as a recepy the slots gets emptyed and a new item is created
             if (isEqual)
             {
                 Debug.Log("AAAAAAAAAAAAAAAAAAAAAA--------------------------------------------------------------");
@@ -63,10 +74,12 @@ public class CrafterManager : MonoBehaviour
                     SlotManager slotRB = item3.GetComponent<SlotManager>();
                     Destroy(slotRB.slotObjectRigidBody.gameObject);
 
+                    //slot gets reset after removing the materials
                     slotRB.slotIsFull = false;
                     slotRB.slotObjectRigidBody = null;
                     slotRB.slotObjectCollider = null;
                 }
+                //new item is created by reading the recepy and adding the ItemTemplate object to the blueprint prefab
                 GameObject newItem = Instantiate(itemBlueprint, new Vector3(transform.position.x, transform.position.y, 50), Quaternion.identity);
                 newItem.GetComponent<ItemDisplay>().item = recipes[System.Array.IndexOf(recipes, item)].itemResult;
             }
@@ -75,6 +88,7 @@ public class CrafterManager : MonoBehaviour
 
     private void ClearArray()
     {
+        //here the items array gets reset to empty
         _itemsInSlot = _itemsInSlotVoid;
     }
 
