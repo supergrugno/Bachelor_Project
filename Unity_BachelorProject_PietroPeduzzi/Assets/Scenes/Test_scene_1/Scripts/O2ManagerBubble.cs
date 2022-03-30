eb0bbeb9c6a8b0641df404e9b5fcdb3b1252887e
ppeduzzi <93376026+supergrugno@users.noreply.github.com>
2022-03-30 16:01:18 +0200

    Sum testing

--- Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/O2ManagerBubble.cs
+++ Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/O2ManagerBubble.cs
@@ -11,6 +11,8 @@ public class O2ManagerBubble : MonoBehaviour
     [SerializeField] GameObject O2_BubbleHead;
     private bool playerIsInBubble = false;
 
+    public bool playerCompletedTutorial = false;
+
     private void Start()
     {
         StaticValues.oxygenInBubble = BubbleStartOxygen;
@@ -19,14 +21,14 @@ public class O2ManagerBubble : MonoBehaviour
 
     private void Update()
     {
-        playerO2ManagerReference.OxygenUsage();
-        playerO2ManagerReference.HPDeploy();
-        RefillPlayerO2();
+            playerO2ManagerReference.OxygenUsage();
+            playerO2ManagerReference.HPDeploy();
+            RefillPlayerO2();
     }
 
     private void RefillPlayerO2()
     {
-        if(playerIsInBubble && StaticValues.oxygenOnPlayer < playerO2ManagerReference.playerMaxOxygen)
+        if(playerIsInBubble && StaticValues.oxygenOnPlayer < playerO2ManagerReference.playerMaxOxygen && playerCompletedTutorial)
         {
             float O2added = playerO2ManagerReference.playerMaxOxygen - StaticValues.oxygenOnPlayer;
             if(StaticValues.oxygenInBubble - O2added >= 0)
@@ -38,6 +40,10 @@ public class O2ManagerBubble : MonoBehaviour
                 StaticValues.oxygenOnPlayer += StaticValues.oxygenInBubble;
                 StaticValues.oxygenInBubble -= StaticValues.oxygenInBubble;
             }
+        }else if(playerIsInBubble && StaticValues.oxygenOnPlayer < playerO2ManagerReference.playerMaxOxygen && !playerCompletedTutorial)
+        {
+            StaticValues.oxygenOnPlayer = playerO2ManagerReference.playerMaxOxygen;
+            if (StaticValues.oxygenInBubble == maxOxygen) playerCompletedTutorial = true;
         }
     }
 
--- Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/PackAnimalMovement.cs
+++ Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/PackAnimalMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PackAnimalMovement : MonoBehaviour
 {
     [SerializeField] private float animalSpeed;
+    private float actualAnimalSpeed = 0;
+    [SerializeField] private O2ManagerBubble o2ManagerBubbleReference;
 
     private void Update()
     {
-        transform.position += Vector3.right * Time.deltaTime * animalSpeed;
+        transform.position += Vector3.right * Time.deltaTime * actualAnimalSpeed;
+        if (o2ManagerBubbleReference.playerCompletedTutorial)
+        {
+            actualAnimalSpeed = animalSpeed;
+        }
     }
 }
