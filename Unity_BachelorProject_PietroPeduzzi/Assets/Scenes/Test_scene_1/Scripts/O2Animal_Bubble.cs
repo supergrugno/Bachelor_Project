c63e77b5ae03f4ba0dccc1b59ee282ca1cb62c55
ppeduzzi_laptop <93376026+supergrugno@users.noreply.github.com>
2022-04-07 17:21:30 +0200

    Bubble dissolve and terrain changes

--- Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/O2Animal_Bubble.cs
+++ Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/O2Animal_Bubble.cs
@@ -11,7 +11,12 @@ public class O2Animal_Bubble : MonoBehaviour
     public O2ManagerBubble o2ManagerBubbleReference;
 
     public GameObject O2_bubble;
+    private Renderer _rend;
 
+    private void Start()
+    {
+        _rend = O2_bubble.GetComponent<Renderer>();
+    }
 
     private void Update()
     {
@@ -25,5 +30,15 @@ public class O2Animal_Bubble : MonoBehaviour
             actualSize = minSize;
             gameObject.transform.localScale = new Vector3(actualSize, actualSize, actualSize);
         }
+        CloseBubbleIfEmpty();
+    }
+
+    private void CloseBubbleIfEmpty()
+    {
+        if(StaticValues.oxygenInBubble <= 10)
+        {
+            _rend.material.SetFloat("_Dissolve", 1-(StaticValues.oxygenInBubble/10));
+        }
+        else if(StaticValues.oxygenInBubble < 10) _rend.material.SetFloat("_Dissolve", 1);
     }
 }
