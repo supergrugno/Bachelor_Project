bdf46634334e2823bf572ffc421f6cae281dade2
ppeduzzi <93376026+supergrugno@users.noreply.github.com>
2022-04-16 17:57:54 +0200

    correzioni

--- Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/PackAnimalMovement.cs
+++ Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/PackAnimalMovement.cs
@@ -7,6 +7,11 @@ public class PackAnimalMovement : MonoBehaviour
     [SerializeField] private float animalSpeed_0 = 0.3f;
     [SerializeField] private float animalSpeed_1 = 0.6f;
     [SerializeField] private float animalSpeed_2 = 0.9f;
+
+    [SerializeField] private float distance_0 = 0.3f;
+    [SerializeField] private float distance_1 = 0.6f;
+    [SerializeField] private float distance_2 = 0.9f;
+
     private float actualAnimalSpeed = 0;
     [SerializeField] private O2ManagerBubble o2ManagerBubbleReference;
 
@@ -22,9 +27,9 @@ public class PackAnimalMovement : MonoBehaviour
         transform.position += Vector3.right * Time.deltaTime * actualAnimalSpeed;
         if (o2ManagerBubbleReference.playerCompletedTutorial)
         {
-            if (StaticValues.distanceTraveled < 100) actualAnimalSpeed = animalSpeed_0;
-            if (StaticValues.distanceTraveled >= 100 && StaticValues.distanceTraveled < 250) actualAnimalSpeed = animalSpeed_1;
-            if (StaticValues.distanceTraveled >= 250 && StaticValues.distanceTraveled < 350) actualAnimalSpeed = animalSpeed_2;
+            if (StaticValues.distanceTraveled < distance_0) actualAnimalSpeed = animalSpeed_0;
+            if (StaticValues.distanceTraveled >= distance_0 && StaticValues.distanceTraveled < distance_2) actualAnimalSpeed = animalSpeed_1;
+            if (StaticValues.distanceTraveled >= distance_2 && StaticValues.distanceTraveled < 350) actualAnimalSpeed = animalSpeed_2;
         }
 
         StaticValues.distanceTraveled = gameObject.transform.position.x - startXPosition;
