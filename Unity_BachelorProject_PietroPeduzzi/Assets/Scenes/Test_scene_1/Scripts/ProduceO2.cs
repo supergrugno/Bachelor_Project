577eab4b8efef511603b3fa537636508e13b47d0
ppeduzzi <93376026+supergrugno@users.noreply.github.com>
2022-05-13 16:57:31 +0200

    BaseCrafter

--- Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/ProduceO2.cs
+++ Unity_BachelorProject_PietroPeduzzi/Assets/Scenes/Test_scene_1/Scripts/ProduceO2.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ProduceO2 : MonoBehaviour
 {
+    [SerializeField] private GameObject O2smokePS;
+
     public void ProduceO2InBubble()
     {
         StaticValues.oxygenInBubble += 10;
         Debug.Log("added O2");
+        Instantiate(O2smokePS, transform.position, Quaternion.identity);
+
     }
 }
